13687a1e30a4ccb26243a60587f529da604f20f5
James Mungai <106020736+JamesMungai254@users.noreply.github.com>
2025-06-22 09:13:38 +0000

    CV update

--- src/components/eductaion.jsx
+++ src/components/eductaion.jsx
@@ -6,16 +6,16 @@ function Education() {
     <section className="education-section" id='education'>
       <h2 className="education-title">Education</h2>
       <p className="education-summary">
-        I am currently pursuing a B.Sc. in Data Science and Analytics and a Software Engineering course at PLP Academy. 
-        I have completed Predictive Analysis at Great Learning and Feature Engineering, as well as Basic and Advanced SQL at Kaggle. 
-        I am also currently engaged in Geospatial Data Science on Kaggle.
+        I am currently pursuing a B.Sc. in Data Science and Analytics and AWS Cloud Practitioner Certification at AWS re/start. 
+        I have completed Associate Data Engineering at Datacamp. 
+        I am also currently interested in cloud computing and how I can integrate it with my current skills.
       </p>
       <ul className="education-list">
         <li><strong>B.Sc. Data Science and Analytics</strong></li>
         <li><strong>Software Engineering</strong> - PLP Academy</li>
         <li><strong>AWS Cloud Practitioning </strong> (in progress) - AWS re/start</li>
       </ul>
-      <a href="https://docs.google.com/document/d/1ZsfZ3FbInb9holWWWfaY60gBaR05a2MU/edit?usp=sharing&ouid=116819448591249776819&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer" className="cv-link">
+      <a href="https://docs.google.com/document/d/16BatDhKQYA33yQnkHp-SHfhEEtCluLCd/edit?usp=sharing&ouid=104770994746378464969&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer" className="cv-link">
         Download my CV
       </a>
     </section>
